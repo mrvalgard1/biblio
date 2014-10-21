6628f735b8c1b86fe9a4e0ab77cd52341cf9ae08
p4welrog <p4wel.rog@gmail.com>
2014-10-21 16:32:42 +0200

    PrimeFaces

--- src/java/pl/altkom/dao/JpaBookDataDAO.java
+++ src/java/pl/altkom/dao/JpaBookDataDAO.java
@@ -54,8 +54,8 @@ public class JpaBookDataDAO implements BookDataDAO {
                 + categoryConstraint +authorConstraint + sortConstraint;
 
         return em.createQuery(query, BookData.class)
-                .setParameter("t", byTitle.toLowerCase() + "%")
-                .setParameter("c", byCategory.toLowerCase() + "%")
+                .setParameter("t", "%" + byTitle.toLowerCase() + "%")
+                .setParameter("c", "%" + byCategory.toLowerCase() + "%")
                 .setParameter("a", "%" + byAuthor.toLowerCase() + "%")
                 .getResultList();
     }
